03134b914f41f2649a9bba906931e27d9912a3c7
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-08 23:55:01 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO DC MOTOR OPERATING WITH SWITCH

---
+++ DC_MOTOR_SW.ino
@@ -0,0 +1,34 @@
+const int buttonPin = 8;   
+const int buttonPin1 = 12;  
+int buttonState = 0,buttonState1 = 0;   
+
+void setup() 
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  PORTB=0x11;
+}
+
+void loop() 
+{
+  buttonState = digitalRead(buttonPin);
+  buttonState1 = digitalRead(buttonPin1);
+  
+  if (buttonState ==LOW) 
+  {     
+    digitalWrite(9, 1);  
+    digitalWrite(10, 0);
+  } 
+  
+  else if (buttonState1 ==LOW) 
+  {     
+    digitalWrite(10, 1);  
+    digitalWrite(9, 0);
+  } 
+  
+  else
+  {
+    digitalWrite(9, 0);  
+    digitalWrite(10, 0);
+  }
+}
